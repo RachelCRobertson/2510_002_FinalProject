c58822cabf85fc8d153002732222a807340ced5a
root <rcrobertso42@tntech.edu>
2026-04-11 01:59:45 +0000

    Add user manually script is complete

--- manualAdd.sh
+++ manualAdd.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+echo "-------------------------------------------------"
+echo "                    Add User                     "
+echo "-------------------------------------------------"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+WHITE='\033[0m'
+
 #Adds a user manually
+echo ""
+echo "Minimum length (3)."
+echo "Maximum length(32)."
+echo "No uppercase letters."
+echo "No special characters."
+echo "Must begin with a lowercase letter."
+read -p "Please Enter your username: " name
+
+while ! [[ "$name" =~ ^[a-z][0-9a-z]{3,32}$ ]]; do
+	echo -e "${RED}Username is invalid.${WHITE}"
+	read -p "Please Enter your username: " name
+done
+
+ sudo adduser $name
