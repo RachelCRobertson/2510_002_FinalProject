15bac00aca36d214d53390d74dfc83d5bfd8ff2f
root <rcrobertso42@tntech.edu>
2026-04-11 19:27:22 +0000

    Added ReadData.sh as a way to rename combined_output.sh

---
+++ ReadData.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scrape(){
+	curl -s https://www.tntech.edu/engineering/programs/csc/faculty-and-staff.php
+	#my script contents
+	| grep -oP '(?<=<h4><strong>)[^,<]*' |
+	 tr '\n' '#' | tr -d '\r' |
+	 sed 's/&nbsp;//g' |
+	 sed 's/#$/\n/'
+}
+
