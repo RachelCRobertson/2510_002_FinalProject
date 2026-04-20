ef7ff9bc5ffe886e3c624e94ca07b323c29464ad
Zachary Seitner <zrseitner42@tntech.edu>
2026-04-20 10:24:50 -0500

    pulled test_main

---
+++ delete_user.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#-----------------------------------------
+# Filename:   delete_user.sh
+# Author:     RedDragon, Mallory Flesher
+# Purpose:    Provides the delete_user()
+#             function. Designed for
+# Testing and demo purposes ONLY. It
+# references the auto_add_user.sh file
+# and deletes every user added by
+# the add_user() function.
+#
+# Functionality:
+#       1. Delete auto added users
+#-----------------------------------------
+
+source ./auto_add_user.sh
+
+GREEN="\033[0;32m"
+WHITE="\033[0m"
+
+delete_user()
+{
+     #counter
+     num=1
+
+     #fully deleting users
+     while IFS= read -r user
+     do
+          echo "$num"
+          ((num++))
+
+          #deleting user
+          sudo deluser --remove-home "$user"
+
+          #checking if delete worked
+          id "$user"
+	  echo -e "${GREEN}$user removed successfully.${WHITE}"
+          echo "-------------------------------------------------"
+     done < username.txt
+
+     #deleting txt file
+     if [ -f "username.txt" ]; then
+          rm "username.txt"
+     fi
+}
+delete_user
---
+++ manual_add.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#-----------------------------------------
+# Filename:   manual_add.sh
+# Author:     RedDragon, Rachel Robertson
+# Purpose:    Provides the m_add()
+#             function that takes a first
+# and last name from input and allows
+# an account to be created manually.
+# A password is then generated for the
+# account and then the account is created.
+#
+# Functionality:
+#       1. Generate password
+#       2. Create an account manually
+#-----------------------------------------
+
+m_add()
+{
+	echo "-------------------------------------------------"
+	echo "                    Add User                     "
+	echo "-------------------------------------------------"
+
+	RED='\033[0;31m'
+	GREEN='\033[0;32m'
+	WHITE='\033[0m'
+
+	# first name validation
+	echo ""
+	read -p "First name: " firstName
+
+	while ! [[ "$firstName" =~ ^[a-zA-Z]+$ ]]; do
+		echo -e "${RED}First name is invalid.${WHITE}"
+		read -p "Please Enter the first name: " firstName
+	done
+
+	# ubuntu only accepts usernames with lowercase letters
+	firstName=${firstName,,}
+
+	# last name validation
+	read -p "Last name: " lastName
+
+	while ! [[ "$lastName" =~ ^[a-zA-Z]+$ ]]; do
+        	echo -e "${RED}First name is invalid.${WHITE}"
+        	read -p "Please Enter the last name: " lastName
+	done
+
+	# ubuntu only accepts usernames with lowercase letters
+	lastName=${lastName,,}
+
+	# puts username and password in the following format:
+	# user-> first.last  pas-> firstlastDEELTECH
+	userName="$firstName"".""$lastName"
+	password="$firstName""$lastName""DEELTECH"
+
+	#adding username to username.txt
+	[ -f username.txt ] || touch username.txt
+	if grep -Fxq "$userName" username.txt; then
+		printf "User %s already exists\n" "$userName"
+	else
+		echo "$userName" >> username.txt
+	fi
+
+	# Adds the user without a password and then changes the password
+	# immediately afterwards.
+	sudo adduser --allow-bad-names  --disabled-password --gecos "" "$userName" 2>&1 | sed 's/^/\t/'
+	echo "$userName:$password" | sudo chpasswd
+
+	# Confirmation
+	echo -e "${GREEN}${userName} added successfully.${WHITE}"
+}
