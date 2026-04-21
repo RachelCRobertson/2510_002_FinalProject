464c3747a163e99bd2a5da85c769273b3b0d0426
root <rcrobertso42@tntech.edu>
2026-04-21 20:49:20 +0000

    Finished escape sequence and allowing multiple deletions, working on the actual deleting process

--- manual_delete_user.sh
+++ manual_delete_user.sh
@@ -11,30 +11,62 @@
 # Functionality:
 #       1. Deletes user
 #-----------------------------------------
+source ./show_all_users.sh
 
 m_del()
 {
+     show_users
      echo "-------------------------------------------------"
      echo "                  Delete User                    "
      echo "-------------------------------------------------"
      echo ""
 
-     #getting user input
-     read -p "Username: " user
+     user=1;
+     while [[ "$user" -ne 0 ]]; do
+     	counter=0;
+
+     	#reading from file and inserting into array
+     	while IFS= read -r user; do
+         	userArr+=("$user")
+	  	counter=$((counter + 1))
+     	done < username.txt
+
+     	#getting user input
+     	echo "0. Exit Delete User"
+     	echo "1-$counter. Delete User"
+     	echo ""
+     	read -p "Please enter the number that corrisponds to the user you want to delete: " user
 
 
-     while true; do
+     	# Checks that the value input is only numbers and is whithin the correct range
+     	# If the user inputs 0 the function ends
+     	while ! [[ "$user" =~ ^[0-9]+$ && "$user" -le $counter && "$user" -ge 0 ]]; do
+		if [[ "$user" = 0 ]]; then
+			return
+		fi
+		echo ""
+		echo "0. Exit Delete User"
+		echo "1-$counter. Delete User"
+                echo ""
+		read -p "Invalid choice, Please enter a number from 1 to $counter: " user
+
+     	done
+	echo ""
+done
+
+     # while loop to check for the desired user
+#     while true; do
           #getting user input
-#          read -p "Username: " user
 
-          if grep -Fxq "$user" username.txt; then
-               sudo deluser --remove-home "$user" 2>&1 | sed 's/^/\t/'
-               echo "-------------------------------------------------"
-               grep -Fxv "$user" username.txt > temp.txt && mv temp.txt username.txt
-               break
-          else
-               echo -e "Username not found."
-               read -p "Please enter the username: " user
-          fi
-     done
+#          if grep -Fxq "$user" username.txt; then
+#               sudo deluser --remove-home "$user" 2>&1 | sed 's/^/\t/'
+#               echo "-------------------------------------------------"
+#               grep -Fxv "$user" username.txt > temp.txt && mv temp.txt username.txt
+#               break
+#          else
+#               echo -e "Username not found."
+#               read -p "Please enter the username: " user
+#          fi
+#     done
 }
+m_del
