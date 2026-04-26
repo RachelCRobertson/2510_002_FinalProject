4fd6c5a0763cd6561777b0b84e85faf211047fd9
root <rcrobertso42@tntech.edu>
2026-04-26 04:45:45 +0000

    Updated documentation on restart.sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+#-----------------------------------------
+# Filename:   restart.sh
+# Author:     RedDragon, Rachel Robertson
+# Purpose:    Restarts main.sh
+#             Restarts main.sh after adding
+# new users so that manual_delete_user.sh
+# can function properly
+#
+# Functionality:
+#       1. Kills and restarts main
+#-----------------------------------------
+
 kill main.sh
  ./main.sh
