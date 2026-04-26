ac16f25ba0feb6ab2a3467326411a72b60110f34
root <rcrobertso42@tntech.edu>
2026-04-26 05:04:38 +0000

    Fixed restart script

---
+++ restart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#-----------------------------------------
+# Filename:   restart.sh
+# Author:     RedDragon, Rachel Robertson
+# Purpose:    Restarts main.sh
+#             Restarts main.sh after adding
+# new users so that manual_delete_user.sh
+# can function properly
+#
+# Functionality:
+#       1. Starts another main
+#-----------------------------------------
+
+echo "Restarting main..."
+ ./main.sh
